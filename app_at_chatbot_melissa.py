a2645b9f77b1eb72648802674fd1ce97c12aae91
Michael Papp <62149488+mwpapp@users.noreply.github.com>
2025-02-21 14:34:17 -0600

    Update app_at_chatbot_melissa.py

--- app_at_chatbot_melissa.py
+++ app_at_chatbot_melissa.py
@@ -15,7 +15,7 @@ This app provides a structured way to interact with an AI-powered clinical chatb
 
 The chatbot has been provided with a student history and a list of functional limitations. Your job is to figure out the primary limitations and give recommendations.
 
-The user will be able to have a free-form conversation with the chatbot to clarify their AT needs. Then they will input their primary challenge and recommendations.
+The user will be able to have a free-form conversation with the chatbot to clarify their AT needs. Then they will input their functional limitations and recommendations.
  """
 
 SHARED_ASSET = {
