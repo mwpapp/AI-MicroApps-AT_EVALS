c0d4af46963de5d8ddac8adc6932104a5a708bae
Michael Papp <62149488+mwpapp@users.noreply.github.com>
2025-02-21 15:04:23 -0600

    Update app_clinical_chatbot.py

--- app_clinical_chatbot.py
+++ app_clinical_chatbot.py
@@ -1,5 +1,5 @@
 APP_URL = "https://melissachatbot-v1.streamlit.app/"
-APP_IMAGE = "at_chatbot_learning.webp"
+APP_IMAGE = "clinical_chatbot_general.webp"
 PUBLISHED = True
 
 ## Set the API key and model name
